5df4d0869f42537043cb2e1d248e607eb904da63
Peter Hemsing <peterhemsing@gmail.com>
2021-02-24 12:53:49 +0100

    setting up wiki api

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    search();
+  }, [term]);
 
 
   return (
