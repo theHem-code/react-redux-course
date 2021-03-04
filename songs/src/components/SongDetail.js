83d19954f1ce8cd3052a33b94b21c126682ed652
Peter Hemsing <peterhemsing@gmail.com>
2021-03-04 12:35:06 +0100

    finishing first redux project

---
+++ songs/src/components/SongDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  // song is the props what we pass to the SongDetail function
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
