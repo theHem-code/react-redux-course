7bab0d8c1121fb1f62cf6798ae68f6e709ad33ba
Peter Hemsing <peterhemsing@gmail.com>
2021-03-23 20:09:46 +0100

    finishing the components

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 
 import {
   SIGN_IN,
@@ -25,8 +26,9 @@ export const signOut = () => {
 
 export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
-  const response = await streams.post("/streams", {...formValues, userId});
+  const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -40,11 +42,14 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
   const response = await streams.delete(`/streams/${id}`);
+
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push("/");
 };
